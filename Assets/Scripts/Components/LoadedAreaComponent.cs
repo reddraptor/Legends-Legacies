a1842aece289429082f921697b635d43cbcf12ba
reddraptor <sdmuir76@gmail.com>
2018-03-03 19:39:25 -0800

    Added LoadedAreaComponent to deal with loading and unloaded chunks

---
+++ Assets/Scripts/Components/LoadedAreaComponent.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Helpers;
+using Assets.Scripts.EditorAttributes;
+
+namespace Assets.Scripts.Components
+{
+    public class LoadedAreaComponent : MonoBehaviour
+    {
+        public int range = 3;
+        [ReadOnly] public int width;
+        public MapComponent map;
+        public LocationComponent location;
+        public Vector2 worldPosition;
+        public bool reload = false;
+
+        private ChunkComponent[,] chunks;
+
+
+        void Load()
+        {
+            ChunkComponent chunk;
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (!(chunk = chunks[i, j])) LoadChunk(new Indices(i, j));
+                }
+            }
+        }
+
+        ChunkComponent LoadChunk(Indices chunkIndices)
+        {
+            ChunkComponent chunkComponent = map.defaultChunk;
+
+            chunkComponent = Instantiate(chunkComponent,
+                new Vector3(
+                    worldPosition.x + (-range + chunkIndices.i) * map.chunkSize,
+                    worldPosition.y + (-range + chunkIndices.j) * map.chunkSize
+                    ),
+                Quaternion.identity, map.transform);
+
+            chunkComponent.loadTilesOutsideViewport = false;
+            chunkComponent.viewPort = map.viewPort;
+            chunkComponent.mapLoadAreaIndices = chunkIndices;
+            chunkComponent.name = chunkComponent.name + "[" + chunkIndices.i + ", " + chunkIndices.j + "]";
+            chunks[chunkIndices.i, chunkIndices.j] = chunkComponent;
+
+            return chunkComponent;
+        }
+
+        void DestroyChunk(ChunkComponent chunk)
+        {
+#if UNITY_EDITOR
+            DestroyImmediate(chunk);
+#else
+            Destroy(chunk);
+#endif
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            map = GetComponentInParent<MapComponent>();
+            location = GetComponent<LocationComponent>();
+            location.map = map;
+            location.mapIndex = map.worldMapIndex;
+            width = range * 2 + 1;
+            chunks = new ChunkComponent[width, width];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            worldPosition = transform.position;
+            location.SetFromWorldPosition(worldPosition);
+            if (reload)
+            {
+                Load();
+                reload = false;
+            }
+        }
+    }
+
+}
--- Assets/Scripts/Components/MapComponent.cs
+++ Assets/Scripts/Components/MapComponent.cs
@@ -17,31 +17,13 @@ namespace Assets.Scripts.Components
         public bool chunkLoadingEnabled = true;
         public Vector2 worldPosition;
         public LocationData.Coordinates centerCoordinates;
-        public LoadedArea loadedArea;
+        public LoadedAreaComponent loadedArea;
         
-        [Serializable]
-        public class LoadedArea
-        {
-            public ChunkComponent[,] chunks;
-         
-            public int range = 3;
-            [ReadOnly]public int width;
-            [ReadOnly]public int chunkSize;
-            public LocationData.Coordinates centerCoordinates;
-
-            public LoadedArea(int chunkSize)
-            {
-                width = range * 2 + 1;
-                chunks = new ChunkComponent[width, width];
-                centerCoordinates = new LocationData.Coordinates(0, 0, chunkSize / 2, chunkSize / 2);
-            }
-        }
-
         // Use this for initialization
         void Start()
         {
             transform.position = Vector2.zero;
-            loadedArea = new LoadedArea(chunkSize);
+            loadedArea = GetComponentInChildren<LoadedAreaComponent>();
             centerCoordinates = new LocationData.Coordinates(0, 0, chunkSize / 2, chunkSize / 2);
         }
 
@@ -49,52 +31,16 @@ namespace Assets.Scripts.Components
         void Update()
         {
             worldPosition = transform.position;
-
+            
             if (viewPort.location.mapIndex == worldMapIndex)
             {
-                loadedArea.centerCoordinates.chunk = viewPort.location.coordinates.chunk;
+                loadedArea.transform.position = viewPort.transform.position;
 
                 if (chunkLoadingEnabled)
                 {
-                    for (int i = 0; i < loadedArea.width; i++)
-                    {
-                        for (int j = 0; j < loadedArea.width; j++)
-                        {
-                            if (!loadedArea.chunks[i, j])
-                                LoadChunk(new Indices(i, j));
-                        }
-                    }
+                    loadedArea.reload = true;
                 }
             }
-        }
-
-        void DestroyChunk(ChunkComponent chunk)
-        {
-#if UNITY_EDITOR
-            DestroyImmediate(chunk);
-#else
-            Destroy(chunk);
-#endif
-        }
-
-        ChunkComponent LoadChunk(Indices areaLoadIndices)
-        {
-            ChunkComponent chunkComponent;
-
-            chunkComponent = Instantiate(defaultChunk,
-                new Vector3(
-                    worldPosition.x - loadedArea.range + (areaLoadIndices.i * chunkSize),
-                    worldPosition.y - loadedArea.range + (areaLoadIndices.j * chunkSize)
-                    ),
-                Quaternion.identity, transform);
-
-            chunkComponent.loadTilesOutsideViewport = false;
-            chunkComponent.viewPort = viewPort;
-            chunkComponent.mapLoadAreaIndices = areaLoadIndices;
-            chunkComponent.name = chunkComponent.name + "[" + areaLoadIndices.i + ", " + areaLoadIndices.j + "]";
-            loadedArea.chunks[areaLoadIndices.i, areaLoadIndices.j] = chunkComponent;
-
-            return chunkComponent;
         }
     }
 
